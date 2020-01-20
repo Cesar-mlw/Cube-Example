53b3341ba30afd3216a0cca79795b587331cb707
Cesar Moura Leite Westphal <cesarmlwestphal@gmail.com>
2020-01-20 10:03:46 -0300

    Ïnitial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour{
+    private bool gameHasEnded = false;
+    public float restartDelay = 2f;
+    public GameObject completeLevelUI;
+    public void EndGame() {
+        if(!gameHasEnded){
+            gameHasEnded = true;
+            Debug.Log("End");
+            Invoke("Restart", restartDelay);
+        }
+    }
+    void Restart() {
+          SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void CompleteLevel() {
+        Debug.Log("Completed!");
+        completeLevelUI.SetActive(true);  
+    }
+}
